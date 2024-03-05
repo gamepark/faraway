25f888f95dc7fcc92e02e39802bf93d1bdc2a431
Romain Fromi <romain.fromi@gmail.com>
2024-03-05 13:38:01 +0100

    Add credits

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <>
       { !!game && <GameDisplay players={game.players.length} /> }
-      <LoadingScreen display={loading} author="Someone" artist="Somebody" publisher="Nobody" developer="You"/>
+      <LoadingScreen display={loading} author={['Johannes Goupy', 'Corentin Lebrat']} artist="Maxime Morin" publisher="Catch Up Games" developer="Game Park"/>
       <MaterialHeader rulesStepsHeaders={Headers} loading={loading}/>
       <Menu/>
       <FailuresDialog/>
