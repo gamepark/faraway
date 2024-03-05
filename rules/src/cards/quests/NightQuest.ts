a8b2e208f1f573fd20729c2f80b0ec92fbbf5cea
Patrick Beaume <p.beaume@gmail.com>
2024-03-05 12:54:51 +0100

    remove console log

--- rules/src/cards/quests/NightQuest.ts
+++ rules/src/cards/quests/NightQuest.ts
@@ -9,7 +9,6 @@ export class NightQuest extends Quest {
   getScore(regions: MaterialItem[], sanctuaries: MaterialItem[]): number | undefined {
     const nightRegions = regions.map((r) => Regions[r.id]?.night ?? 0)
     const nightSanctuaries = sanctuaries.map((r) => Sanctuaries[r.id]?.night ?? 0)
-    console.log(sum(nightRegions) + sum(nightSanctuaries))
     return this.points * (sum(nightRegions) + sum(nightSanctuaries))
   }
 }
