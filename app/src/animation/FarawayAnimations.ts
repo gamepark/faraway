a49e892c8702ec245511e4de0f3e2662adcb6dd4
Patrick Beaume <p.beaume@gmail.com>
2024-03-10 21:34:51 +0100

    review animation duration

--- app/src/animation/FarawayAnimations.ts
+++ app/src/animation/FarawayAnimations.ts
@@ -1,7 +1,21 @@
+import { LocationType } from '@gamepark/faraway/material/LocationType'
+import { MaterialType } from '@gamepark/faraway/material/MaterialType'
 import { MaterialGameAnimations } from '@gamepark/react-game'
-import { isShuffle } from '@gamepark/rules-api'
+import { isMoveItemType, isShuffle } from '@gamepark/rules-api'
 
 export const farawayAnimations = new MaterialGameAnimations()
+
+farawayAnimations.when()
+  .move((move) => isMoveItemType(MaterialType.Region)(move) && move.location.type === LocationType.Region)
+  .duration(0.5)
+
+farawayAnimations.when()
+  .move((move) => isMoveItemType(MaterialType.Region)(move) && move.location.type === LocationType.RegionDiscard)
+  .duration(0.5)
+
+farawayAnimations.when()
+  .move((move) => isMoveItemType(MaterialType.Sanctuary)(move) && move.location.type === LocationType.SanctuaryDeck)
+  .duration(0.8)
 
 farawayAnimations.when()
   .move(isShuffle)
