08635e377b2e3bf42ea2de8f5d6b982ef303d737
Romain Fromi <romain.fromi@gmail.com>
2024-03-12 14:04:32 +0100

    Prevent hidden region card help from crashing the game

--- app/src/material/help/RegionCardHelp.tsx
+++ app/src/material/help/RegionCardHelp.tsx
@@ -2,45 +2,53 @@
 import { css } from '@emotion/react'
 import { CardDescription } from '@gamepark/faraway/cards/CardDescription'
 import { Color } from '@gamepark/faraway/cards/Color'
-import { getColor, getValue } from '@gamepark/faraway/cards/Region'
+import { getColor, getValue, Region } from '@gamepark/faraway/cards/Region'
 import { Regions } from '@gamepark/faraway/cards/Regions'
 import { Wonder } from '@gamepark/faraway/cards/Wonder'
 import { MaterialHelpProps, Picture } from '@gamepark/react-game'
 import { Trans, useTranslation } from 'react-i18next'
 import autumn from '../../images/icon/autumn.jpg'
 import chimera from '../../images/icon/chimera.png'
-import clue from '../../images/icon/clue.png'
+import clueIcon from '../../images/icon/clue.png'
 import rock from '../../images/icon/rock.png'
 import spring from '../../images/icon/spring.jpg'
 import summer from '../../images/icon/summer.jpg'
 import thistle from '../../images/icon/thistle.png'
 import winter from '../../images/icon/winter.jpg'
-import night from '../../images/time/night.png'
+import nightIcon from '../../images/time/night.png'
 
 export const RegionCardHelp = ({ item }: MaterialHelpProps) => {
   const { t } = useTranslation()
-  const number = getValue(item.id)
-  const color = getColor(item.id)
-  const region = Regions[item.id] as CardDescription
+  const number = item.id ? getValue(item.id) : ''
   return <>
     <h2>{t('help.region', { number })}</h2>
+    {item.id && <RegionHelp region={item.id}/>}
+  </>
+}
+
+const RegionHelp = ({ region }: { region: Region }) => {
+  const { t } = useTranslation()
+  const number = getValue(region)
+  const color = getColor(region)
+  const { night, clue, wonders } = Regions[region] as CardDescription
+  return <>
     <p><strong>{t('help.time', { number })}</strong></p>
     <p><em>{t('help.time.detail')}</em></p>
-    {region.night &&
+    {night &&
       <p css={alignIcon}>
-        <Picture src={night}/>&nbsp;{t('help.night')}
+        <Picture src={nightIcon}/>&nbsp;{t('help.night')}
       </p>
     }
     <p css={alignIcon}><Picture css={css`border-radius: 20%;`} src={biomeIcon[color]}/>&nbsp;<strong>{t(`biome.${color}`)}</strong></p>
     <p><em>{t('help.biome')}</em></p>
-    {region.clue &&
+    {clue &&
       <p css={alignIcon}>
-        <Picture src={clue}/>&nbsp;<Trans defaults="help.clue"><strong/></Trans>
+        <Picture src={clueIcon}/>&nbsp;<Trans defaults="help.clue"><strong/></Trans>
       </p>
     }
-    {region.wonders &&
+    {wonders &&
       <p css={alignIcon}>
-        {region.wonders.map(wonder => <Picture src={wonderIcon[wonder]}/>)}&nbsp;<Trans defaults="help.wonders"><strong/><em/></Trans>
+        {wonders.map(wonder => <Picture src={wonderIcon[wonder]}/>)}&nbsp;<Trans defaults="help.wonders"><strong/><em/></Trans>
       </p>
     }
   </>
